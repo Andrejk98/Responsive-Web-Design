5fff47e671f76fa6325530fa81124db5a5b8c5ed
Andrej Kalinic <andrejk98@web.de>
2024-04-23 23:05:59 +0200

    Änderungen zurück gebaut

--- Code/index.js
+++ Code/index.js
@@ -4,7 +4,7 @@ function handleMediaQuery(mediaQuery) {
         console.log("Media Query ist erfüllt");
         // Füge hier deinen JavaScript-Code ein, der innerhalb der Media Query ausgeführt werden soll
 
-
+        
         // Wähle alle Elemente mit der Klasse .bild-text-box aus
         const parentElements = document.querySelectorAll('.bild-text-box');
 
@@ -13,11 +13,9 @@ function handleMediaQuery(mediaQuery) {
             // Wähle die Elemente innerhalb des aktuellen übergeordneten Elements aus
             const textElement = parentElement.querySelector('.text');
             const bildElement = parentElement.querySelector('.bild');
-            // Überprüfe, ob das Textelement vor dem Bildelement ist
-            if (textElement.compareDocumentPosition(bildElement) & Node.DOCUMENT_POSITION_PRECEDING) {
-                // Füge das Bild-Element vor dem Text-Element ein
-                parentElement.insertBefore(bildElement, textElement);
-            }
+
+            // Füge das Bild-Element vor dem Text-Element ein
+            parentElement.insertBefore(bildElement, textElement);
         });
         
     } 
@@ -25,14 +23,16 @@ function handleMediaQuery(mediaQuery) {
         // Media Query ist nicht erfüllt
         console.log("Media Query ist nicht erfüllt");
         // Füge hier deinen JavaScript-Code ein, um die Änderungen rückgängig zu machen
+
         // Wähle alle Elemente mit der Klasse .bild-text-box aus
         const parentElements = document.querySelectorAll('.bild-text-box');
+
         // Iteriere über jedes übergeordnete Element
         parentElements.forEach(parentElement => {
             // Wähle die Elemente innerhalb des aktuellen übergeordneten Elements aus
             const textElement = parentElement.querySelector('.text');
             const bildElement = parentElement.querySelector('.bild');
-            // Überprüfe, ob das Textelement vor dem Bildelement ist
+
             if (textElement.compareDocumentPosition(bildElement) & Node.DOCUMENT_POSITION_FOLLOWING) {
                 // Füge das Text-Element vor dem Bild-Element ein
                 parentElement.insertBefore(textElement, bildElement);
@@ -40,8 +40,10 @@ function handleMediaQuery(mediaQuery) {
         });
     }
 }
+
 // Definiere die Media Query, auf die du reagieren möchtest
 const mediaQuery = window.matchMedia("(max-width: 720px)");
+
 // Führe die Funktion beim Laden der Seite aus und füge einen Event-Listener hinzu,
 // um auf Änderungen der Media Query zu reagieren
 handleMediaQuery(mediaQuery);
