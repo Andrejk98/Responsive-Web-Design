c09e8169dbca7f841642874e61c99653f6e3b110
Andrej Kalinic <andrejk98@web.de>
2024-04-23 10:06:06 +0200

    Leichte Änderungen in der HTML und Javascript funktionsfähig, dass Bild vor Text angezeigt wird

---
+++ Code/index.js
@@ -0,0 +1,37 @@
+function handleMediaQuery(mediaQuery) {
+    if (mediaQuery.matches) {
+        // Media Query ist erfüllt
+        console.log("Media Query ist erfüllt");
+        // Füge hier deinen JavaScript-Code ein, der innerhalb der Media Query ausgeführt werden soll
+
+        
+        // Wähle alle Elemente mit der Klasse .bild-text-box aus
+        const parentElements = document.querySelectorAll('.bild-text-box');
+
+        // Iteriere über jedes übergeordnete Element
+        parentElements.forEach(parentElement => {
+            // Wähle die Elemente innerhalb des aktuellen übergeordneten Elements aus
+            const textElement = parentElement.querySelector('.text');
+            const bildElement = parentElement.querySelector('.bild');
+
+            // Füge das Bild-Element vor dem Text-Element ein
+            parentElement.insertBefore(bildElement, textElement);
+        });
+        
+    } 
+    else {
+        // Media Query ist nicht erfüllt
+        console.log("Media Query ist nicht erfüllt");
+    }
+}
+
+
+
+
+// Definiere die Media Query, auf die du reagieren möchtest
+const mediaQuery = window.matchMedia("(max-width: 768px)");
+
+// Führe die Funktion beim Laden der Seite aus und füge einen Event-Listener hinzu,
+// um auf Änderungen der Media Query zu reagieren
+handleMediaQuery(mediaQuery);
+mediaQuery.addListener(handleMediaQuery);
