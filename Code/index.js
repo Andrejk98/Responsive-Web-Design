e2e18ca400a4ae48dc831d8d586416f94a030be0
Andrej Kalinic <andrejk98@web.de>
2024-06-11 10:04:45 +0200

    Bild Karusell eingefügt und stylistische änderungen eingefügt

--- Code/index.js
+++ Code/index.js
@@ -39,3 +39,49 @@ mediaQuery.addListener(handleMediaQuery);
 
 // Code für Vorhernachher Slider
 $('.ba-slider').beforeAfter();
+
+//Bilder Karussell
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    /* setTimeout(showSlides, 3000); */ // Wechselt alle 3 Sekunden
+}
+
+function plusSlides(n) {
+    showSpecificSlide(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSpecificSlide(slideIndex = n);
+}
+
+function showSpecificSlide(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
