cad512bac58076c37839b5a8b66675581ceca9e5
Andrej Kalinic <andrejk98@web.de>
2024-06-20 22:47:35 +0200

    Sounds und Bilder für Controller eingefügt

--- Code/index.js
+++ Code/index.js
@@ -28,7 +28,39 @@ function handleMediaQuery(mediaQuery1, mediaQuery2) {
         restoreOriginalOrder();
     }
 }
+/* hovern von controllern */
+document.addEventListener('DOMContentLoaded', function() {
+    const boxes = document.querySelectorAll('.box');
+    const isTouchDevice = 'ontouchstart' in window || navigator.msMaxTouchPoints;
 
+    boxes.forEach(box => {
+        const soundId = box.getAttribute('data-sound');
+        const sound = document.getElementById(soundId);
+
+        if (isTouchDevice) {
+            box.addEventListener('click', function() {
+                if (sound) {
+                    sound.currentTime = 0; // Rewind to start of audio
+                    sound.play();
+                }
+            });
+        } else {
+            box.addEventListener('mouseenter', function() {
+                if (sound) {
+                    sound.currentTime = 0; // Rewind to start of audio
+                    sound.play();
+                }
+            });
+
+            box.addEventListener('mouseleave', function() {
+                if (sound) {
+                    sound.pause();
+                    sound.currentTime = 0; // Zurücksetzen auf den Anfang des Audios für das nächste Mal
+                }
+            });
+        }
+    });
+});
 // Definiere die Media Queries, auf die du reagieren möchtest
 const mediaQuery1 = window.matchMedia("(max-width: 720px)");
 const mediaQuery2 = window.matchMedia("(max-width: 1024px)");
@@ -42,7 +74,6 @@ mediaQuery2.addListener(() => handleMediaQuery(mediaQuery1, mediaQuery2));
 $('.ba-slider').beforeAfter();
 
 //Bilder Karussell
-
 let slideIndex = 0;
 showSlides();
 
@@ -60,7 +91,7 @@ function showSlides() {
     }
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
-    /* setTimeout(showSlides, 3000); */ // Wechselt alle 3 Sekunden
+    // setTimeout(showSlides, 3000); // Wechselt alle 3 Sekunden
 }
 
 function plusSlides(n) {
@@ -86,6 +117,27 @@ function showSpecificSlide(n) {
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
 }
+
+// Add touch functionality
+let startX;
+
+document.addEventListener('touchstart', function(event) {
+    startX = event.touches[0].clientX;
+}, false);
+
+document.addEventListener('touchend', function(event) {
+    let endX = event.changedTouches[0].clientX;
+    let deltaX = endX - startX;
+    if (Math.abs(deltaX) > 50) { // Adjust the threshold as needed
+        if (deltaX > 0) {
+            // Swipe right
+            plusSlides(-1);
+        } else {
+            // Swipe left
+            plusSlides(1);
+        }
+    }
+}, false);
 
 /* Header Bild */
 document.addEventListener('DOMContentLoaded', function() {
